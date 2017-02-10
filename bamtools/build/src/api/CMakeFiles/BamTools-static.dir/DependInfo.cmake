b3dc9359082aa621f27652c327ab9821fab20ded
LEE <el114@clarinet000-199.orchestra>
2017-02-09 21:47:36 -0500

    modified bamtools added

---
+++ bamtools/build/src/api/CMakeFiles/BamTools-static.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/BamAlignment.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/BamAlignment.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/BamMultiReader.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/BamMultiReader.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/BamReader.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/BamReader.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/BamWriter.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/BamWriter.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/SamHeader.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/SamHeader.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/SamProgram.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/SamProgram.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/SamProgramChain.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/SamProgramChain.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/SamReadGroup.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/SamReadGroup.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/SamReadGroupDictionary.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/SamReadGroupDictionary.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/SamSequence.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/SamSequence.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/SamSequenceDictionary.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/SamSequenceDictionary.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/bam/BamHeader_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/bam/BamHeader_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/bam/BamMultiReader_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/bam/BamMultiReader_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/bam/BamRandomAccessController_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/bam/BamRandomAccessController_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/bam/BamReader_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/bam/BamReader_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/bam/BamWriter_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/bam/BamWriter_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/index/BamIndexFactory_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/index/BamIndexFactory_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/index/BamStandardIndex_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/index/BamStandardIndex_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/index/BamToolsIndex_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/index/BamToolsIndex_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/BamDeviceFactory_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BamDeviceFactory_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/BamFile_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BamFile_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/BamFtp_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BamFtp_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/BamHttp_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BamHttp_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/BamPipe_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BamPipe_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/BgzfStream_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BgzfStream_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/ByteArray_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/ByteArray_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/HostAddress_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/HostAddress_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/HostInfo_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/HostInfo_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/HttpHeader_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/HttpHeader_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/ILocalIODevice_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/ILocalIODevice_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/RollingBuffer_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/RollingBuffer_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/TcpSocketEngine_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/TcpSocketEngine_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/TcpSocketEngine_unix_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/TcpSocketEngine_unix_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/io/TcpSocket_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/TcpSocket_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/sam/SamFormatParser_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/sam/SamFormatParser_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/sam/SamFormatPrinter_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/sam/SamFormatPrinter_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/sam/SamHeaderValidator_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/sam/SamHeaderValidator_p.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/api/internal/utils/BamException_p.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools-static.dir/internal/utils/BamException_p.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "BAMTOOLS_API_LIBRARY"
+  "_FILE_OFFSET_BITS=64"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_convert.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_convert.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_count.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_count.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_coverage.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_coverage.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_filter.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_filter.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_header.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_header.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_index.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_index.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_merge.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_merge.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_random.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_random.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_resolve.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_resolve.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_revert.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_revert.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_sort.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_sort.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_split.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_split.cpp.o"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/src/toolkit/bamtools_stats.cpp" "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_stats.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_FILE_OFFSET_BITS=64"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/api/CMakeFiles/BamTools.dir/DependInfo.cmake"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/utils/CMakeFiles/BamTools-utils.dir/DependInfo.cmake"
+  "/n/no_backup/park/elim/kyu/eclipse/workspaces/genomics/bamtools/build/src/third_party/jsoncpp/CMakeFiles/jsoncpp.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/api"
+  "../src/utils"
+  "../src/third_party"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
